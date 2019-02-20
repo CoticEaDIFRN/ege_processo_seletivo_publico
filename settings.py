dacef7a0677f54736202266abcdbb5df71b87d27
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-02-20 20:14:08 -0300

    [UPD] Rename module name from ege_auth_jwt to ege_utils

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 
 # Apps
-MY_APPS = env_as_list('MY_APPS', 'inscricao,ege_auth_jwt,ege_theme')
+MY_APPS = env_as_list('MY_APPS', 'inscricao,ege_utils,ege_theme')
 DEV_APPS = env_as_list('DEV_APPS', 'debug_toolbar,django_extensions' if DEBUG else '')
 # THIRD_APPS = env_as_list('THIRD_APPS', 'ege_theme')
 THIRD_APPS = env_as_list('THIRD_APPS', 'rest_framework')
@@ -120,6 +120,6 @@ EGE_ACESSO_JWT_VALIDATE = env("EGE_ACESSO_JWT_VALIDATE", 'http://acesso:8000/ege
 EGE_ACESSO_JWT_LOGOUT = env("EGE_ACESSO_JWT_LOGOUT", 'http://acesso:8000/ege/acesso/logout/')
 EGE_ACESSO_JWT_CLIENT_ID = env("EGE_ACESSO_JWT_CLIENT_ID", '_EGE_ACESSO_JWT_CLIENT_ID_')
 EGE_ACESSO_JWT_SECRET = env("EGE_ACESSO_JWT_SECRET", '_EGE_ACESSO_JWT_SECRET_')
-EGE_AUTH_JWT_BACKEND = env("EGE_AUTH_JWT_BACKEND", 'ege_auth_jwt.backends.PreExistentUserJwtBackend')
+EGE_UTILS_AUTH_JWT_BACKEND = env("EGE_UTILS_AUTH_JWT_BACKEND", 'ege_utils.backends.PreExistentUserJwtBackend')
 
 AUTH_USER_MODEL = env("auth_USER_MODEL", 'inscricao.Candidato')
